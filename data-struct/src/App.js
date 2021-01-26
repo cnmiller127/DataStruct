1e3c7bc94fd6973f2b4ae7b0eead567db52319a5
Christian Miller <cnmiller127@gmail.com>
2021-01-25 22:52:12 -0600

    deploy

--- data-struct/src/App.js
+++ data-struct/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Array from "./pages/Array";
 import Tree from "./pages/Tree";
 //import Nav from "./components/Nav";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavEx from "./components/Nav/index.js";
 import "./Style.css";
@@ -10,15 +10,11 @@ import "./Style.css";
 
 function App() {
   return (
-  <Router>
-    <NavEx />
     <div className = {""}>
-        <Switch>
-          <Route exact path = {["/array", "/"]} component = {Array} />
-           <Route exact path = {"/tree"} component = {Tree}/>
-        </Switch>
+    <NavEx />
+          <Route exact path = "/array" component = {Array} />
+          <Route exact path = "/tree" component = {Tree}/>
     </div>
-  </Router>
   );
 }
 
